17e9a38438534c174a0e14fae1087dc099411667
Johnnathan Cubillos <john.cubillos@alumnos.duoc.cl>
2021-01-23 08:44:57 -0300

    Agregado y reemplazado los mantenedores html para su uso con python flask

--- app.py
+++ app.py
@@ -9,49 +9,52 @@ app = Flask(__name__)
 #Rutear el home (la raiz del sitio)
 @app.route('/')
 
-
 def Index():
+    return render_template('Index.html')
+
+#--------------configurar registroCliente como inicio
+#def Index():
     #return señala cual es la pagina que va levantar
     #return render_template('Index.html') #levanta la pagina home
 
     #guarda una coleccion con los datos 
-    datos = mantenedorCliente.consultar()
+    #datos = mantenedorCliente.consultar()
     #renderiza la pagina clientes y a la vez manda los datos almacenados en un arreglo clientes
-    return render_template('registroCliente.html',clientes=datos)
+    #return render_template('registroCliente.html',clientes=datos)
 
     #renderizacion de la pagina limpia
     #return render_template('registroCliente.html')
-
+#------------------------------------------
 #route de la accion del formulario y agregar el metodo del formulario
 #metodo post esconde los datos
 #metodo get muestra los datos
-@app.route('/ingresarCliente',methods=['POST'])
+# @app.route('/ingresarCliente',methods=['POST'])
 
-def ingresarCliente():
-    if request.method == 'POST':
-        #CREATE
-        try:
-            auxBotonRegistrar = request.form['btnRegistrar']
-            if auxBotonRegistrar == 'Registrar':
-                auxRut = request.form['txtRut']
-                auxNombe = request.form['txtNombre']
-                auxApellido = request.form['txtApellido']
-                auxTelefono = request.form['txtTelefono']
-                auxCorreo = request.form['txtCorreo']
-                auxDireccion = request.form['txtDireccion']
-                auxComuna = request.form['txtComuna']
-                auxRegion = request.form['txtRegion']
-                auxCliente = claseCliente.Cliente(0,auxNombe,auxApellido,auxRut,auxTelefono,auxCorreo,auxDireccion,auxComuna,auxRegion)
-                mantenedorCliente.insertar(auxCliente)
-                print("Datos guardados")
-                #flash("Datos Guardados")
+# def ingresarCliente():
+#     if request.method == 'POST':
+#         #CREATE
+#         try:
+#             auxBotonRegistrar = request.form['btnRegistrar']
+#             if auxBotonRegistrar == 'Registrar':
+#                 auxRut = request.form['txtRut']
+#                 auxNombe = request.form['txtNombre']
+#                 auxApellido = request.form['txtApellido']
+#                 auxTelefono = request.form['txtTelefono']
+#                 auxCorreo = request.form['txtCorreo']
+#                 auxDireccion = request.form['txtDireccion']
+#                 auxComuna = request.form['txtComuna']
+#                 auxRegion = request.form['txtRegion']
+#                 auxCliente = claseCliente.Cliente(0,auxNombe,auxApellido,auxRut,auxTelefono,auxCorreo,auxDireccion,auxComuna,auxRegion)
+#                 mantenedorCliente.insertar(auxCliente)
+#                 print("Datos guardados")
+#                 #flash("Datos Guardados")
 
-        except:
-            print("Datos no guardados")
-            #flash("Datos no guardados")
-        #es necesario redireccionar para que no se caiga luego de insertar
-        return redirect(url_for('Index'))
-
+#         except:
+#             print("Datos no guardados")
+#             #flash("Datos no guardados")
+#         #es necesario redireccionar para que no se caiga luego de insertar
+#         return redirect(url_for('Index'))
+#----------------------------Fin registrar datos-------------
 
 if __name__ == '__main__':
     #agregar un puerto que no se ocupe 500,5000,300,etc
